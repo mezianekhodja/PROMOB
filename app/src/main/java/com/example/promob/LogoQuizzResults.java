17d5cd636c2b3cbfcb777bcbdacda76b1b7b7ddf
mezianekhodja <meziane.khodja@etudiant.univ-rennes1.fr>
2020-10-09 20:10:38 +0200

    page resultat version 1

--- app/src/main/java/com/example/promob/LogoQuizzResults.java
+++ app/src/main/java/com/example/promob/LogoQuizzResults.java
@@ -2,13 +2,36 @@ package com.example.promob;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
+//si jamais on veut finir comme ca
 public class LogoQuizzResults extends AppCompatActivity {
+
+    Button btn_suivant;
+    int score;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logo_quizz_results);
+        score = getIntent().getExtras().getInt("score");
+        Toast.makeText(this,String.valueOf(score),Toast.LENGTH_LONG).show();
+        btn_suivant = (Button) findViewById(R.id.buttonsvteasyres);
+        btn_suivant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openHomeLogoQuizz();
+            }
+        });
+    }
+    public void openHomeLogoQuizz() {
+        Intent intent = new Intent(this, HomeLogoQuizz.class);
+        startActivity(intent);
     }
 }
