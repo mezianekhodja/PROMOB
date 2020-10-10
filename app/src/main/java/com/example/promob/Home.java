dafe2d911cf4bab2be40b8622e8c68a5bdb16e4a
mezianekhodja <meziane.khodja@etudiant.univ-rennes1.fr>
2020-10-10 20:36:46 +0200

    phase de connexion basique (à améliorer) (pour se connecter :Admin,mdp)

---
+++ app/src/main/java/com/example/promob/Home.java
@@ -0,0 +1,31 @@
+package com.example.promob;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Home extends AppCompatActivity {
+
+    Button btn_logoquizz;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        btn_logoquizz = (Button) findViewById(R.id.button_logoquizz);
+        btn_logoquizz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openHomeLogoQuizz();
+            }
+        });
+    }
+
+    public void openHomeLogoQuizz() {
+        Intent intent = new Intent(this, HomeLogoQuizz.class);
+        startActivity(intent);
+    }
+}
