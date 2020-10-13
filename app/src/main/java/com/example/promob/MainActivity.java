4c7ba07d1c8afc526a2146889cb87dc14b8e2694
mezianekhodja <meziane.khodja@etudiant.univ-rennes1.fr>
2020-10-13 15:17:24 +0200

    avancé

--- app/src/main/java/com/example/promob/MainActivity.java
+++ app/src/main/java/com/example/promob/MainActivity.java
@@ -6,25 +6,45 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity  {
-    Button btn_logoquizz;
+    private EditText name;
+    private EditText password;
+    private TextView info;
+    private Button login;
+    private int counter = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn_logoquizz = (Button) findViewById(R.id.button_logoquizz);
-        btn_logoquizz.setOnClickListener(new View.OnClickListener() {
+
+        name = (EditText) findViewById(R.id.editTextTextPersonName);
+        password = (EditText) findViewById(R.id.editTextTextPassword);
+        info = (TextView)findViewById(R.id.info);
+        login = (Button)findViewById(R.id.buttonlog);
+
+        login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openHomeLogoQuizz();
+                validate(name.getText().toString(),password.getText().toString());
             }
         });
     }
 
-    public void openHomeLogoQuizz() {
-        Intent intent = new Intent(this, HomeLogoQuizz.class);
-        startActivity(intent);
+    private void validate(String username, String password){
+        if((username.equals("Admin"))&&(password.equals("mdp"))){
+            Intent intent = new Intent(this, Home.class);
+            startActivity(intent);
+        }
+        else{
+        counter--;
+        info.setText("nombre d'essais restants = "+String.valueOf(counter));
+        }
+        if(counter == 0){
+           login.setEnabled(false);
+        }
     }
 }
