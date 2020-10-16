fbf72a5e018501842c504f633674e204f70cae5b
mezianekhodja <meziane.khodja@etudiant.univ-rennes1.fr>
2020-10-16 20:42:08 +0200

    possibilité de déconnexion

--- app/src/main/java/com/example/promob/Home.java
+++ app/src/main/java/com/example/promob/Home.java
@@ -1,20 +1,27 @@
 package com.example.promob;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Home extends AppCompatActivity {
 
     Button btn_logoquizz;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        firebaseAuth = FirebaseAuth.getInstance();
         btn_logoquizz = (Button) findViewById(R.id.button_logoquizz);
         btn_logoquizz.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,5 +34,29 @@ public class Home extends AppCompatActivity {
     public void openHomeLogoQuizz() {
         Intent intent = new Intent(this, HomeLogoQuizz.class);
         startActivity(intent);
+    }
+    public void openActivityConnexion() {
+        Intent intent = new Intent(this, Connexion.class);
+        startActivity(intent);
+    }
+    private void Logout() {
+        firebaseAuth.signOut();
+        finish();
+        openActivityConnexion();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.logoutMenu:{
+                Logout();
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
