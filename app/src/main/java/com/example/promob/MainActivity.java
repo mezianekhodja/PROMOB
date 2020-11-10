8d3c3c1078c0baf4043d6303065cddacbf2f6ad9
mezianekhodja <meziane.khodja@etudiant.univ-rennes1.fr>
2020-11-10 15:12:02 +0100

    correction pb invite

--- app/src/main/java/com/example/promob/MainActivity.java
+++ app/src/main/java/com/example/promob/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.promob;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,13 +8,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity  {
     private Button connexion,invite;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        firebaseAuth = FirebaseAuth.getInstance();
 
         connexion = (Button)findViewById(R.id.btn_P1_connexion);
         invite = (Button)findViewById(R.id.btn_P1_invite);
@@ -27,7 +36,7 @@ public class MainActivity extends AppCompatActivity  {
         invite.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openInvite();
+                validate("invitevmgames@gmail.com","inviteVMG");
             }
         });
     }
@@ -37,10 +46,23 @@ public class MainActivity extends AppCompatActivity  {
         MainActivity.this.finish();
     }
 
-    public void openInvite() {
-        Intent intent = new Intent(this, Entrainement.class);
+    private void validate(String username, String password){
+       FirebaseUser user = firebaseAuth.getCurrentUser();
+        if (user != null){
+            firebaseAuth.signOut();
+        }
+        firebaseAuth.signInWithEmailAndPassword(username, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    openHome();
+                }
+            }
+        });
+    }
+    private void openHome(){
+        Intent intent = new Intent(this, Solo_Multi.class);
         startActivity(intent);
         MainActivity.this.finish();
     }
-
 }
