08eb9d9cb3c2ad849eedccd0cd099880d116aae5
meziane <meziane.khodja@etudiant.univ-rennes1.fr>
2020-11-17 13:54:31 +0100

    gestion liste rect + differents niveaux

--- app/src/main/java/com/example/promob/LabyrintheGame.java
+++ app/src/main/java/com/example/promob/LabyrintheGame.java
@@ -5,12 +5,15 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
 
 //Composant grafique (View)
 public class LabyrintheGame extends View implements SensorEventListener {
@@ -28,6 +31,10 @@ public class LabyrintheGame extends View implements SensorEventListener {
     //position affichage balle
     private int currentX;
     private int currentY;
+    //liste de nos rectangles representant le chemin
+    private List<Rect> listeChemin = new ArrayList<>();
+    //ajustement valeurs en fonction du niveau
+    int centerwidht=520,intervalwidht=100;
 
     public LabyrintheGame(Context context){
         super(context);
@@ -51,6 +58,40 @@ public class LabyrintheGame extends View implements SensorEventListener {
         //w et h sont la taille et hauteur de l'ecran
         currentX =  (w - imageWidth)/2;
         currentY =  (h - imageHeight);
+
+        //création des chemins en fonction des niveaux de difficulté (plusieurs chemins possibles par niveau)
+        //chemin niveau facile
+        Rect rchemeasy1A = new Rect(centerwidht-intervalwidht,10,centerwidht+intervalwidht,1350);
+        //listeChemin.add(rchemeasy1);
+
+        //chemin niveau moyen
+        //chemin 1 niveau moyen
+        /*
+        Rect rchemed1A = new Rect(centerwidht-intervalwidht,1000,centerwidht+intervalwidht,1350);
+        listeChemin.add(rchemed1A);
+        Rect rchemed1B = new Rect(centerwidht-intervalwidht,10,centerwidht+intervalwidht,700);
+        listeChemin.add(rchemed1B);
+        Rect rchemed1C = new Rect(centerwidht-intervalwidht,500,centerwidht+3*intervalwidht,700);
+        listeChemin.add(rchemed1C);
+        Rect rchemed1D = new Rect(centerwidht-intervalwidht,1000,centerwidht+3*intervalwidht,1200);
+        listeChemin.add(rchemed1D);
+        Rect rchemed1E = new Rect(centerwidht+intervalwidht,500,centerwidht+3*intervalwidht,1200);
+        listeChemin.add(rchemed1E);
+         */
+
+        //chemin 2 niveau moyen
+        Rect rchemed2A = new Rect(centerwidht-intervalwidht,1000,centerwidht+intervalwidht,1350);
+        listeChemin.add(rchemed2A);
+        Rect rchemed2B = new Rect(centerwidht-intervalwidht,10,centerwidht+intervalwidht,700);
+        listeChemin.add(rchemed2B);
+        Rect rchemed2C = new Rect(centerwidht-3*intervalwidht,500,centerwidht-intervalwidht,700);
+        listeChemin.add(rchemed2C);
+        Rect rchemed2D = new Rect(centerwidht-3*intervalwidht,1000,centerwidht-intervalwidht,1200);
+        listeChemin.add(rchemed2D);
+        Rect rchemed2E = new Rect(centerwidht-3*intervalwidht,500,centerwidht-intervalwidht,1200);
+        listeChemin.add(rchemed2E);
+
+        //chemin niveau difficile
     }
 
 
@@ -58,21 +99,31 @@ public class LabyrintheGame extends View implements SensorEventListener {
     //appele a chaque fois que l'ecran devra se re actualiser
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
         //reglage couleurs arrivees
         Paint paintmodif = new Paint();
-        //affichage background
+
+        //affichage delimitation background
         paintmodif.setARGB(255,0,0,0);
-        canvas.drawRect(0,0,1100,1400,paintmodif);
+        canvas.drawRect(0,0,1100,10,paintmodif);
+
         //affichage chemin
         paintmodif.setARGB(255,255,255,255);
-        canvas.drawRect(420,0,620,1400,paintmodif);
+        if (!listeChemin.isEmpty()){
+            for(int i = 0 ; i < listeChemin.size(); i++){
+                canvas.drawRect(listeChemin.get(i),paintmodif);
+            }
+        }
+
         //affichage arrivée
         paintmodif.setARGB(255,255,0,0);
-        canvas.drawRect(420,0,620,50,paintmodif);
+        canvas.drawRect(centerwidht-intervalwidht,10,centerwidht+intervalwidht,50,paintmodif);
+
         //affichage départ
         paintmodif.setARGB(255,0,255,0);
-        canvas.drawRect(420,1300,620,1400,paintmodif);
-        //Sur la zone graphique du composant, on affiche l'image
+        canvas.drawRect(centerwidht-intervalwidht,1270,centerwidht+intervalwidht,1320,paintmodif);
+
+        //affichage bille
         canvas.drawBitmap(ballBitmap, currentX, currentY, paint);
     }
 
