e8e2b52cff590ad7d6b1e462f9777e5125812ed7
meziane <meziane.khodja@etudiant.univ-rennes1.fr>
2020-12-02 23:07:22 +0100

    blocage accès multi si utilisateur invite

--- app/src/main/java/com/example/promob/Solo_Multi.java
+++ app/src/main/java/com/example/promob/Solo_Multi.java
@@ -1,15 +1,28 @@
 package com.example.promob;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class Solo_Multi extends AppCompatActivity  {
     private Button solo,multi;
+    private FirebaseDatabase firebaseDatabase;
+    private FirebaseAuth firebaseAuth;
+    private DatabaseReference databaseReference;
+    private String username ="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +42,25 @@ public class Solo_Multi extends AppCompatActivity  {
             @Override
             public void onClick(View v) {
                 openMulti();
+            }
+        });
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference(firebaseAuth.getUid());
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                UserProfile userProfile = snapshot.getValue(UserProfile.class);
+                username=userProfile.getUserName();
+                if (username.equals("invite")){
+                    multi.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(Solo_Multi.this, error.getCode(),Toast.LENGTH_SHORT).show();
             }
         });
     }
