22b1c227f86f66fbdc6f96d73e43141e8b5ef463
meziane <meziane.khodja@etudiant.univ-rennes1.fr>
2020-12-13 11:43:03 +0100

    ajout page bande son

--- app/src/main/java/com/example/promob/BandeSon.java
+++ app/src/main/java/com/example/promob/BandeSon.java
@@ -4,21 +4,174 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class BandeSon extends AppCompatActivity {
     private FirebaseAuth firebaseAuth;
+    private Button run1,run2,run3,run4,run5,run6,run7,stop1,stop2,stop3,stop4,stop5,stop6,stop7;
+    MediaPlayer play;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bande_son);
         firebaseAuth = FirebaseAuth.getInstance();
+        run1 = findViewById(R.id.buttonRunMusique1);
+        run1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startMusique1();
+            }
+        });
+        run2 = findViewById(R.id.buttonRunMusique1);
+        run2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startMusique2();
+            }
+        });
+        run3 = findViewById(R.id.buttonRunMusique1);
+        run3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startMusique3();
+            }
+        });
+        run4 = findViewById(R.id.buttonRunMusique1);
+        run4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startMusique4();
+            }
+        });
+        run5 = findViewById(R.id.buttonRunMusique1);
+        run5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startMusique5();
+            }
+        });
+        run6 = findViewById(R.id.buttonRunMusique1);
+        run6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startMusique6();
+            }
+        });
+        run7 = findViewById(R.id.buttonRunMusique1);
+        run7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startMusique7();
+            }
+        });
+        stop1 = findViewById(R.id.buttonStopMusique1);
+        stop1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopMusic();
+            }
+        });
+        stop2 = findViewById(R.id.buttonStopMusique2);
+        stop2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopMusic();
+            }
+        });
+        stop3 = findViewById(R.id.buttonStopMusique3);
+        stop3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopMusic();
+            }
+        });
+        stop4 = findViewById(R.id.buttonStopMusique4);
+        stop4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopMusic();
+            }
+        });
+        stop5 = findViewById(R.id.buttonStopMusique5);
+        stop5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopMusic();
+            }
+        });
+        stop6 = findViewById(R.id.buttonStopMusique6);
+        stop6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopMusic();
+            }
+        });
+        stop7 = findViewById(R.id.buttonStopMusique7);
+        stop7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopMusic();
+            }
+        });
 
+    }
+    public void stopMusic(){
+        play.stop();
+        Toast.makeText(this, "Musique arrêtée", Toast.LENGTH_SHORT).show();
+    }
+    public void startMusique1(){
+        if(play == null){
+            play = MediaPlayer.create(this, R.raw.musique1);
+            Toast.makeText(this, "Musique 1 lancée", Toast.LENGTH_SHORT).show();
+        }
+        play.start();
+    }
+    public void startMusique2(){
+        if(play == null){
+            play = MediaPlayer.create(this, R.raw.musique2);
+            Toast.makeText(this, "Musique lancée", Toast.LENGTH_SHORT).show();
+        }
+        Toast.makeText(this, "Problème", Toast.LENGTH_SHORT).show();
+        play.start();
+    }public void startMusique3(){
+        if(play == null){
+            play = MediaPlayer.create(this, R.raw.musique3);
+            Toast.makeText(this, "Musique lancée", Toast.LENGTH_SHORT).show();
+        }
+        play.start();
+    }public void startMusique4(){
+        if(play == null){
+            play = MediaPlayer.create(this, R.raw.musique4);
+            Toast.makeText(this, "Musique lancée", Toast.LENGTH_SHORT).show();
+        }
+        play.start();
+    }public void startMusique5(){
+        if(play == null){
+            play = MediaPlayer.create(this, R.raw.musique5);
+            Toast.makeText(this, "Musique lancée", Toast.LENGTH_SHORT).show();
+        }
+        play.start();
+    }public void startMusique6(){
+        if(play == null){
+            play = MediaPlayer.create(this, R.raw.musique6);
+            Toast.makeText(this, "Musique lancée", Toast.LENGTH_SHORT).show();
+        }
+        play.start();
+    }public void startMusique7(){
+        if(play == null){
+            play = MediaPlayer.create(this, R.raw.musique7);
+            Toast.makeText(this, "Musique lancée", Toast.LENGTH_SHORT).show();
+        }
+        play.start();
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
